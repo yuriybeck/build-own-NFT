e811f01cf35a652adb2496a688444d565195cfd8
Yuriy Solovyov <dev@solovyov.de>
2022-01-29 10:50:13 +0100

    refactored contract

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -28,7 +28,8 @@ contract MyEpicNFT is ERC721URIStorage {
     _safeMint(msg.sender, newItemId);
 
     // Set the NFTs data.
-    _setTokenURI(newItemId, "blah");
+    _setTokenURI(newItemId, "https://i.solovyov.de/metadata.json");
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
     // Increment the counter for when the next NFT is minted.
     _tokenIds.increment();
